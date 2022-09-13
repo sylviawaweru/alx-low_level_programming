86823a57545ba5e386a8e623c40889f1d483a69d
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-13 08:17:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<main.h>
+#include"main.h"
 /**
  * main - Print _putchar, followed by a new line.
  *
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
+	putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
 		putchar('\n');
 		return (0);
 }
