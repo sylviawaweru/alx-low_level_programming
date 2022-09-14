e7b1ed553045f9dbfb8a64ef72effe31327fe15b
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-14 08:30:55 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int x2;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (x2 = 'a'; x2 <= 'z'; x2++)
+		{
+		_putchar(x2);
+		}
+		_putchar(\'n')
+	}
+}
