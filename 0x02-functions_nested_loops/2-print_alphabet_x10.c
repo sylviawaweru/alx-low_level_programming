f823648d2d6bb81740057606bf2cc3aacd700cf5
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-14 08:43:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,12 +13,12 @@ void print_alphabet_x10(void)
 	int x;
 	int x2;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x = 9; x++)
 	{
 		for (x2 = 'a'; x2 <= 'z'; x2++)
 		{
-		_putchar(x2);
+			_putchar(x2);
 		}
-		_putchar(\'n')
+		_putchar('\n');
 	}
 }
