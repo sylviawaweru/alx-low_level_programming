b82f957581fee2059c0d6a8a17692fed94de0e3b
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-15 11:11:04 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+			if (c != '2' && c != '4')
+			{
+				_putchar(c);
+			}
+		c++;
+		c++;
+	}
+	_putchar('\n');
+}
