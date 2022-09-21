cb821715bea6ba28b81d8ec9cbcbdb0dd24a4197
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-21 05:26:21 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: *ptr pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char src[10] = "Hello";
+	char dest[10] = "world";
+
+	char *ptr
+
+		strncat(src, dest);
+	return (*ptr);
+}
