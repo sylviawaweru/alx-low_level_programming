631808cf343c426be80cbe5a86826be4aca9c900
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-22 03:34:15 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,25 +5,26 @@
  * @dest: the destination string
  * @src: the source string
  *
- * Return: dest
+ * Return: A ponter to the returning string dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++
-	}
 	j = 0;
-	while (dest[j] != '\0')
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-	j++;
-	i++;
+		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
+
 	return (dest);
 }
