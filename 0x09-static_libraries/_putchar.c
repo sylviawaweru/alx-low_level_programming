72436168a3ca7dcc32746d254a416bb00940b2d6
sylviawaweru <sylviawaweru.sw@gmail.com>
2022-09-30 14:24:03 -0700

    finally updated Create the static library libmy.a containing all the functions listed

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,13 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: on success 1,-1 on fail
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write (1, &c , 1));
+	return (write(1, &c, 1));
 }
